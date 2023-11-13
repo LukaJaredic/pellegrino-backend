5f245c8dbe4cfa9737201397590ce10fe7592311
Luka Jaredic <luka.jaredic@effectiva.hr>
2023-11-13 14:13:58 +0100

    test: return original email adress

--- src/api/offer-request/content-types/offer-request/lifecycles.js
+++ src/api/offer-request/content-types/offer-request/lifecycles.js
@@ -17,7 +17,7 @@ module.exports = {
 
     try {
       await strapi.plugins["email"].services.email.send({
-        to: "projaredic@gmail.com",
+        to: "pellegrinots@gmail.com",
         subject: "A new offer request has been received",
         html: `
         <h1>A new offer request has been received at ${createdAt}</h1>
